279a867d95600787096b83d25be8a00ccff4f43c
Li Yong Pan <michelpan@hotmail.com>
2023-09-03 01:07:46 +0200

    conditional rendering of succes and error page

--- app.js
+++ app.js
@@ -5,18 +5,10 @@ const { readFileSync } = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.set("view engine", "ejs");
 
 const sftp = new SftpClient({
-  debug: true,
+  //   debug: true,
 });
 
 const sftpConfig = {
@@ -27,20 +19,33 @@ const sftpConfig = {
   username: process.env.SFTP_USERNAME,
   privateKey: readFileSync("C:/Users/Li Yong/.ssh/id_ed25519"), // Use this for key-based authentication
   passphrase: process.env.SSH_PASSPHRASE,
-//   debug: console.log,
+  //   debug: console.log,
   tryKeyboard: true,
-  retries: 1,
+  retries: 0,
+  readyTimeout: 1000,
 };
 
 async function connect() {
   try {
-    console.log(sftpConfig);
     console.log("Trying to connect to SFTP server...");
     await sftp.connect(sftpConfig);
     console.log("Connected to SFTP server");
+    return true;
   } catch (error) {
     console.error("Error:", error.message);
+    return false;
   }
 }
 
-connect();
+app.get("/", async (req, res) => {
+  const isConnected = await connect();
+  if (isConnected) {
+    res.render("home");
+  } else {
+    res.render("error");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
