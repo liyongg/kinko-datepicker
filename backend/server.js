eb5691d38a27a181db8d44653d4cb8b7954f1593
liyongg <44399190+liyongg@users.noreply.github.com>
2024-03-08 01:16:24 +0100

    ci: use hardcoded port 3101

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const SftpClient = require("ssh2-sftp-client");
 const { readFileSync, writeFileSync, existsSync, mkdirSync } = require("fs");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3101;
 
 // Middleware
 app.use(cors());
